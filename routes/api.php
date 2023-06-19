0c6cae7148c02b78f90416055bc23be7a6f85a20
Shaiju PM <shaiju.pm@infolitz.com>
2023-06-19 12:15:29 +0530

    product model and migration added

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('products',function(){
+    return 'products';
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
